0bc8793c1674c891965ce7ed41ba9f9b3e91967c
Gustavo Henrique <Gustavohgbzxc123@gmail.com>
2023-10-08 11:40:45 -0300

    Update 2006-Identificando o Chá.py

--- Python_Exercices/2006-Identificando o Chá.py
+++ Python_Exercices/2006-Identificando o Chá.py
@@ -1,16 +1,5 @@
 cha = int(input())
-count = 0 
-a, b, c, d, e = map(int, input().split())
+lista = list(map(int, input().split()))
 
-if cha == a:
-    count += 1
-if cha == b:
-    count += 1
-if cha == c:
-    count += 1
-if cha == d:
-    count += 1
-if cha == e:
-    count += 1
-
-print(count)
+y = lista.count(cha)
+print(y)
